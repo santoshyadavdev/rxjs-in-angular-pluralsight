7a73cacf9a00c714797fd7035701f402e67c09ec
santoshyadav198613 <santosh.yadav198613@gmail.com>
2019-07-19 11:47:40 +0530

    added of and from

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { of, from } from 'rxjs';
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   pageTitle = 'Acme Product Management';
+
+  ngOnInit() {
+    of(2, 4, 6, 8).subscribe(console.log);
+
+    from([5, 10, 15, 20]).subscribe(
+      item => console.log(`resulting item ${item}`),
+      err => console.error(`error occurred ${err}`),
+      () => console.log('complete')
+    );
+
+    from(['user1', 'user2', 'user3']).subscribe(
+      item => console.log(`resulting item ${item}`),
+      err => console.error(`error occurred ${err}`),
+      () => console.log('complete')
+    );
+
+    of('user1', 'user2', 'user3').subscribe(
+      item => console.log(`resulting item ${item}`),
+      err => console.error(`error occurred ${err}`),
+      () => console.log('complete')
+    );
+  }
+
 }
