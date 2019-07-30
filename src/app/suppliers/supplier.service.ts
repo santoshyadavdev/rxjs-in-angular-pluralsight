326951d675e4c9b197f13f63f1c4bb9f4a8ce85b
santoshyadav198613 <santosh.yadav198613@gmail.com>
2019-07-30 15:17:38 +0530

    added concatMap

--- src/app/suppliers/supplier.service.ts
+++ src/app/suppliers/supplier.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { throwError } from 'rxjs';
+import { throwError, of } from 'rxjs';
+import { concatMap, tap } from 'rxjs/operators';
+import { Supplier } from './supplier';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,16 @@ import { throwError } from 'rxjs';
 export class SupplierService {
   suppliersUrl = 'api/suppliers';
 
-  constructor(private http: HttpClient) { }
+  supplierWithConcatMap$ = of(1,5,8).pipe(
+    tap(id => console.log('concatMap source observable', id)),
+    concatMap(id=> this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+  );
+
+  constructor(private http: HttpClient) { 
+    this.supplierWithConcatMap$.subscribe(
+      item=> console.log('concatMap result', item)
+    )
+  }
 
   private handleError(err: any) {
     // in a real world app, we may send the server to some remote logging infrastructure
